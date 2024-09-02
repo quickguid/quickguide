583556215ebf8a740bdbf296f895517b16526180
quickguid <asaidrous@gmail.com>
2024-09-02 16:01:25 +0300

    Updated config to use JawsDB

--- website/__init__.py
+++ website/__init__.py
@@ -4,13 +4,15 @@ from flask_login import LoginManager
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
+
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Ar0966678@localhost/adobe5'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('mysql://fb7d44wlpxshifyo:so77r3i5bv82rt89@d6q8diwwdmy5c9k9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/jjtl9wgwpao1sh9w')  # Use JawsDB URL
     app.config['USER_EMAIL_SENDER_EMAIL'] = 'Ar0966678@gmail.com'
     app.config['USER_ENABLE_EMAIL'] = True
 
@@ -21,21 +23,18 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-   
 
     from .models import User
-    
+
     with app.app_context():
         db.create_all()
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
-    login = LoginManager()
-    login.init_app(app)
 
-
-    @login.user_loader
+    @login_manager.user_loader
     def load_user(id):
-        return User.query.get(int(id))  #if this changes to a string, remove int
+        return User.query.get(int(id))  # if this changes to a string, remove int
+    
     return app
